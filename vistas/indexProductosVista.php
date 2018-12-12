1669e9acc23fb3aa0b83ec4b959ebe07334ad330
Pau Marques <pmarquesgu@fp.uoc.edu>
2018-12-12 20:39:57 +0100

    boton valorar y css

--- vistas/indexProductosVista.php
+++ vistas/indexProductosVista.php
@@ -8,12 +8,12 @@
         if(isset($valorado)){
        session_start();
      $valorado=unserialize($_SESSION['valorado']);
-        }
+        }       
       ?>
-
+    
 <section class="seccion clearfix">
 	<h1>Ultimas peliculas valoradas</h1>
-
+       
 <div id="box">
 
     <?php
@@ -21,34 +21,34 @@
        if($producto->getIdProducto() != 0){ ?>
 
     <div class="item_box">
-        <?php foreach($producto->getImagenes() as $imagen) {
+        <?php foreach($producto->getImagenes() as $imagen) { 
             if($imagen->getNombreImagen() == "Portada"){
             ?>
         <img src="<?php echo $imagen->getDirectorioImagen();  ?>" alt="Portada"/>
-
-            <?php } }?>
+        
+            <?php } }?> 
             <div class="foto_miniatura clearfix">
-            <?php foreach($producto->getImagenes() as $imagen) {
+            <?php foreach($producto->getImagenes() as $imagen) {  
             if($imagen->getNombreImagen()=="Escena") { ?>
-
+        
           <div class="foto">
           <img src="<?php echo $imagen->getDirectorioImagen();  ?>" alt="Escena"/>
-
+          
           </div><!--div .foto-->
             <?php } }  ?>
       </div><!--div .foto_miniatura-->
-
-
+      
+        
       <div class="item_title">
-
+      
       <?php if($_SESSION['login']==true){ ?>
-
-                <a href="?controller=Valoraciones&action=nuevaValoracion&id=<?php echo $producto->getIdProducto();?>">
+      
+                 
                 <span><?php echo $producto->getNombreProducto();  ?></span>
-        </a>
+        
         <?php }else{ ?>
                 <span><?php echo $producto->getNombreProducto();  ?></span>
-
+        
         <?php } ?>
       </div>
 
@@ -57,31 +57,36 @@
       </div>
 
       <div class="item_valoracion">
-        <form>
-            <?php
+        
+            <?php 
             $valoracion_media=$producto->getValoracionMedia();
             $media= round($valoracion_media->getValorVotacion() / $valoracion_media->getNumeroVotaciones());
-
+            
             ?>
-			<h4>Valoración media <?php echo $media;  ?></h4>
-                        <p><?php echo $valoracion_media->getVotos();  ?></p>
+			<h4>Valoración media </h4>
+                                             
+                        
   <h2 class="clasificacion">
-    <?php
+   <?php
     $cont=0;
-
+                        
     while($cont<$media){
-    echo "<label>★</label>";
+    echo "<span class='estrella_fija'>★</span>";
     $cont ++;
     }
    ?>
-  </h2>
-</form>
-      </div>
-      <?php $valoraciones=$producto->getValoracionesBBDD($producto->getIdProducto()); ?>
-      <?php foreach ($valoraciones as $valoracion){
-                    $usuario=new UsuariosModelo();
-                    $usuario=$usuario->getById($valoracion->getIdUsuario());
+  </h2>              
 
+      </div>
+      <div class='item_desc'>
+           <a href='?controller=Valoraciones&action=nuevaValoracion&id=<?php echo $producto->getIdProducto();?>' class='buttom_green'>Valorar</a>
+    </div> 
+      <?php $valoraciones=$producto->getValoracionesBBDD($producto->getIdProducto()); ?>
+      <?php foreach ($valoraciones as $valoracion){  
+                    $usuario=new UsuariosModelo();
+                    $id_usuario=$valoracion->getIdUsuario();
+                    $usuario=$usuario->getById($id_usuario);
+                    
                     ?>
 		<div class="item_comentarios clearfix">
 		<div class="autor_comentario">
@@ -92,30 +97,30 @@
 			</div>
 			<div class="item_valoracion clearfix">
 			<div class="valor_valoracion">
-				<form>
-  <p class="clasificacion">Valor votación:<?php echo $valoracion->getValorVotacion(); ?>
+				
+  <p class="clasificacion">Valor votación:
     <?php
     $cont=0;
     while($valoracion->getValorVotacion()>$cont){
-      echo "<label>★</label>";
+      echo "<span class='estrella_fija'>★</span>";
       $cont ++;
     }
-
+    
     ?>
   </p>
-</form>
+
 				</div>
 				<div class="fecha_valoracion">
 				<p> <?php echo $valoracion->getFechaValoracion();  ?></p>
 				</div>
 			</div>
-
+      
 		</div>
       <?php } ?>
     </div><!--.item_box-->
 
-
-
+      
+      
    <?php } }  ?>
 </div><!--#BOX-->
 </section>
